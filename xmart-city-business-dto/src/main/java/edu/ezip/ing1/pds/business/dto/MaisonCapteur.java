9773e318fc4540d0fa12641e459d21c5123680ad
Axel JACOMME <axelj.1312@gmail.com>
2025-03-08 09:46:11 +0100

    Push d'une classe pour se rapprocher du prototype

---
+++ xmart-city-business-dto/src/main/java/edu/ezip/ing1/pds/business/dto/MaisonCapteur.java
@@ -0,0 +1,99 @@
+package edu.ezip.ing1.pds.business.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+import java.lang.reflect.Field;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+@JsonRootName(value = "capteurs")
+public class MaisonCapteur {
+    private  String nameCapteur;
+    private  String typeCapteur;
+    private  String etat;
+    private int idCapteur;
+
+    public MaisonCapteur() {
+    }
+    public final MaisonCapteur build(final ResultSet resultSet)
+            throws SQLException, NoSuchFieldException, IllegalAccessException {
+        setFieldsFromResulset(resultSet, "nameCapteur", "typeCapteur", "etat", "idCapteur");
+        return this;
+    }
+    public final PreparedStatement build(PreparedStatement preparedStatement)
+            throws SQLException, NoSuchFieldException, IllegalAccessException {
+        return buildPreparedStatement(preparedStatement, nameCapteur, typeCapteur,etat);
+    }
+    public MaisonCapteur(String nameCapteur, String typeCapteur, String etat, int idCapteur) {
+        this.nameCapteur = nameCapteur;
+        this.typeCapteur = typeCapteur;
+        this.etat = etat;
+        this.idCapteur = idCapteur;
+    }
+    public String getName() {
+        return nameCapteur;
+    }
+
+    public String getTypecapteur() {
+        return typeCapteur;
+    }
+
+    public String getEtat() {
+        return etat;
+    }
+
+    public int getIdCapteur() {
+        return idCapteur;
+    }
+
+    @JsonProperty("capteur_name")
+    public void setName(String nameCapteur) {
+        this.nameCapteur = nameCapteur;
+    }
+
+    @JsonProperty("capteur_typecapteur")
+    public void setTypecapteur(String typeCapteur) {
+        this.typeCapteur = typeCapteur;
+    }
+
+    @JsonProperty("capteur_etat")
+    public void setEtat(String etat) {
+        this.etat = etat;
+    }
+
+    @JsonProperty("capteur_id")
+    public void setIdCapteur(int idCapteur) {
+        this.idCapteur = idCapteur;
+    }
+
+    private void setFieldsFromResulset(final ResultSet resultSet, final String ... fieldNames )
+            throws NoSuchFieldException, SQLException, IllegalAccessException {
+        for(final String fieldName : fieldNames ) {
+            final Field field = this.getClass().getDeclaredField(fieldName);
+            field.set(this, resultSet.getObject(fieldName));
+        }
+    }
+
+    private final PreparedStatement buildPreparedStatement(PreparedStatement preparedStatement, final String ... fieldNames )
+            throws NoSuchFieldException, SQLException, IllegalAccessException {
+        int ix = 0;
+        for(final String fieldName : fieldNames ) {
+            preparedStatement.setString(++ix, fieldName);
+        }
+        return preparedStatement;
+    }
+
+    @Override
+    public String toString() {
+        return "Capteur{" +
+                "name='" + nameCapteur + '\'' +
+                ", type='" + typeCapteur + '\'' +
+                ", etat='" + etat + '\'' +
+                ", id='" + idCapteur + '\'' +
+                '}';
+    }
+
+}
