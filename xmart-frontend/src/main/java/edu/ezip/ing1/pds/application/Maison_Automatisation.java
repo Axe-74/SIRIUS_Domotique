74197047f3e9394a6245983ad847620d19223c93
Dupuis Enzo <enzo.dupuis76@gmail.com>
2025-03-11 12:20:45 +0100

    déplacement de l'application dans le frontend

---
+++ xmart-frontend/src/main/java/edu/ezip/ing1/pds/application/Maison_Automatisation.java
@@ -0,0 +1,63 @@
+package edu.ezip.ing1.pds.application;
+
+import java.util.Observable;
+
+public class Maison_Automatisation extends Observable {
+    String NomAutomatisation;
+    static TypeCapteurs TypeCapteurs;
+    static TypeProgramme TypeProgramme;
+
+
+    public Maison_Automatisation(String nomautomatisation, TypeCapteurs typecapteurs, TypeProgramme typeprogramme) {
+        NomAutomatisation = nomautomatisation ;
+        TypeCapteurs = typecapteurs;
+        TypeProgramme = typeprogramme;
+    }
+
+    public Maison_Automatisation() {
+
+    }
+
+    public void NomAutomatisation(String string) {
+    }
+
+    public void TypeCapteurs(String string) {
+
+    }
+
+
+    public enum TypeCapteurs {
+        CAPTEURS_1, CAPTEURS_2, CAPTEURS_3, CAPTEURS_4
+    };
+
+    public enum TypeProgramme {
+        PROGRAMME_1, PROGRAMME_2,PROGRAMME_3
+    };
+
+    public void setNomProgramme(String nomautomatisation) {
+        NomAutomatisation = nomautomatisation;
+    };
+
+
+    //Retourne le type de piece
+    public static TypeCapteurs findByNameTypeCapteurs(String name) {
+        TypeCapteurs result = null;
+        for (TypeCapteurs typePiece : TypeCapteurs.values())
+            if (typePiece.name().equalsIgnoreCase(name)) {
+                result = typePiece;
+                break;
+            }
+        return result;
+    }
+
+    //Retourne le type de chauffage
+    public static TypeProgramme findByNameTypeProgramme(String name) {
+        TypeProgramme result = null;
+        for (TypeProgramme typeChauffage : TypeProgramme.values())
+            if (typeChauffage.name().equalsIgnoreCase(name)) {
+                result = typeChauffage;
+                break;
+            }
+        return result;
+    }
+}
