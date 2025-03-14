0fe592b7a23a6128fa14cfae4242063993586af5
TDR917 <tomdarocha917@gmail.com>
2025-03-14 22:10:58 +0100

    roomservice

---
+++ xmart-frontend/src/main/java/edu/ezip/ing1/pds/services/RoomService.java
@@ -0,0 +1,100 @@
+package edu.ezip.ing1.pds.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import edu.ezip.commons.LoggingUtils;
+import edu.ezip.ing1.pds.business.dto.Room;
+import edu.ezip.ing1.pds.business.dto.Rooms;
+import edu.ezip.ing1.pds.client.commons.ClientRequest;
+import edu.ezip.ing1.pds.client.commons.ConfigLoader;
+import edu.ezip.ing1.pds.client.commons.NetworkConfig;
+import edu.ezip.ing1.pds.commons.Request;
+import edu.ezip.ing1.pds.requests.InsertRoomsClientRequest;
+import edu.ezip.ing1.pds.requests.SelectAllRoomsClientRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
+
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.UUID;
+
+public class RoomService {
+    private final static String LoggingLabel = "FrontEnd - RoomService";
+    private final static Logger logger = LoggerFactory.getLogger(LoggingLabel);
+    private final static String roomsToBeInserted = "rooms-to-be-inserted.yaml";
+
+    final String insertRequestOrder = "INSERT_ROOM";
+    final String selectRequestOrder = "SELECT_ALL_ROOMS";
+
+
+    private final NetworkConfig networkConfig;
+
+    public RoomService(NetworkConfig networkConfig) {
+        this.networkConfig = networkConfig;
+    }
+
+    public void insertRooms() throws InterruptedException, IOException {
+        final Deque<ClientRequest> clientRequests = new ArrayDeque<ClientRequest>();
+        final Rooms guys = ConfigLoader.loadConfig(Rooms.class, roomsToBeInserted);
+
+        int birthdate = 0;
+        for(final Room guy : guys.getRooms()) {
+            final ObjectMapper objectMapper = new ObjectMapper();
+            final String jsonifiedGuy = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(guy);
+            logger.trace("Room with its JSON face : {}", jsonifiedGuy);
+            final String requestId = UUID.randomUUID().toString();
+            final Request request = new Request();
+            request.setRequestId(requestId);
+            request.setRequestOrder(insertRequestOrder);
+            request.setRequestContent(jsonifiedGuy);
+            objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
+            final byte []  requestBytes = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsBytes(request);
+
+            final InsertRoomsClientRequest clientRequest = new InsertRoomsClientRequest(
+                    networkConfig,
+                    birthdate++, request, guy, requestBytes);
+            clientRequests.push(clientRequest);
+        }
+
+        while (!clientRequests.isEmpty()) {
+            final ClientRequest clientRequest = clientRequests.pop();
+            clientRequest.join();
+            final Room guy = (Room)clientRequest.getInfo();
+            logger.debug("Thread {} complete : {} {} {} --> {}",
+                    clientRequest.getThreadName(),
+                    guy.getType(), guy.getName(), guy.getSurface(),
+                    clientRequest.getResult());
+        }
+    }
+
+    public Rooms selectRooms() throws InterruptedException, IOException {
+        int birthdate = 0;
+        final Deque<ClientRequest> clientRequests = new ArrayDeque<ClientRequest>();
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final String requestId = UUID.randomUUID().toString();
+        final Request request = new Request();
+        request.setRequestId(requestId);
+        request.setRequestOrder(selectRequestOrder);
+        objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
+        final byte []  requestBytes = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsBytes(request);
+        LoggingUtils.logDataMultiLine(logger, Level.TRACE, requestBytes);
+        final SelectAllRoomsClientRequest clientRequest = new SelectAllRoomsClientRequest(
+                networkConfig,
+                birthdate++, request, null, requestBytes);
+        clientRequests.push(clientRequest);
+
+        if(!clientRequests.isEmpty()) {
+            final ClientRequest joinedClientRequest = clientRequests.pop();
+            joinedClientRequest.join();
+            logger.debug("Thread {} complete.", joinedClientRequest.getThreadName());
+            return (Rooms) joinedClientRequest.getResult();
+        }
+        else {
+            logger.error("No rooms found");
+            return null;
+        }
+    }
+
+}
