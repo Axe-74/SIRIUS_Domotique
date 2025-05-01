1d77baa4cf46dfca236661e0209a9827a924541d
Axel JACOMME <axelj.1312@gmail.com>
2025-05-01 20:46:07 +0200

    Ajout de Service pour la classe Paramètres des types capteurs

---
+++ xmart-frontend/src/main/java/edu/ezip/ing1/pds/services/CapteurParaTypeService.java
@@ -0,0 +1,59 @@
+package edu.ezip.ing1.pds.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import edu.ezip.commons.LoggingUtils;
+import edu.ezip.ing1.pds.business.dto.Capteur_Para_Types;
+import edu.ezip.ing1.pds.client.commons.ClientRequest;
+import edu.ezip.ing1.pds.client.commons.NetworkConfig;
+import edu.ezip.ing1.pds.commons.Request;
+import edu.ezip.ing1.pds.requests.SelectAllParaTypeCapteurClientRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
+
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.UUID;
+
+public class CapteurParaTypeService {
+    private final static String LoggingLabel = "FrontEnd - CapteurParaTypeService";
+    private final static Logger logger = LoggerFactory.getLogger(LoggingLabel);
+
+    final String selectRequestOrder = "SELECT_ALL_NAME";
+
+    private final NetworkConfig networkConfig;
+
+    public CapteurParaTypeService(NetworkConfig networkConfig) throws InterruptedException {
+        this.networkConfig = networkConfig;
+    }
+
+    public Capteur_Para_Types select_all_name() throws InterruptedException, IOException {
+        int birthdate = 0;
+        final Deque<ClientRequest> clientRequests = new ArrayDeque<ClientRequest>();
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final String requestId = UUID.randomUUID().toString();
+        final Request request = new Request();
+        request.setRequestId(requestId);
+        request.setRequestOrder(selectRequestOrder);
+        objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
+        final byte[] requestBytes = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsBytes(request);
+        LoggingUtils.logDataMultiLine(logger, Level.TRACE, requestBytes);
+        final SelectAllParaTypeCapteurClientRequest clientRequest = new SelectAllParaTypeCapteurClientRequest(
+                networkConfig,
+                birthdate++, request, null, requestBytes);
+        clientRequests.push(clientRequest);
+
+        if (!clientRequests.isEmpty()) {
+            final ClientRequest joinedClientRequest = clientRequests.pop();
+            joinedClientRequest.join();
+            logger.debug("Thread {} complete.", joinedClientRequest.getThreadName());
+            return (Capteur_Para_Types) joinedClientRequest.getResult();
+        } else {
+            logger.error("Pas de type de capteur trouvé");
+            return null;
+        }
+    }
+}
+
